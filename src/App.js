b0f2b46f9fda7c3d9903c8eb4bb930b75f2767da
Debbie <dbarros64@gmail.com>
2021-05-27 18:22:36 -0700

    created CustomButton and sign in and sign up form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Switch, Route } from 'react-router-dom';
 
 import Homepage from './Pages/Homepage/Homepage';
 import ShopPage from './Pages/ShopPage/ShopPage';
+import Header from './Components/Header/Header';
+import SignInAndSignUp from './Pages/Sign-In-And-Sign-Up/SignInAndSignUp';
 import './App.css';
 
 
@@ -11,9 +13,11 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <Header />
         <Switch>
           <Route exact path='/' component={Homepage} />
           <Route path='/hats' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUp}/>
         </Switch>
       </div>
     )
