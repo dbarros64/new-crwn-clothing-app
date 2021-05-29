b9b5cda2d428d8bdda683e8d60dedf2753f820a3
Debbie <dbarros64@gmail.com>
2021-05-29 08:32:43 -0700

    finished sign in and handle submit method

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import FormInput from '../FormInput/FormInput';
 import CustomButton from '../CustomButton/CustomButton';
-import { signInWithGoogle } from '../../Firebase/Firebase.config';
+import { auth, signInWithGoogle } from '../../Firebase/Firebase.config';
 
 
 import './SignIn.styles.scss';
@@ -18,10 +18,18 @@ class SignIn extends React.Component {
     }
 
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
 
-        this.setState({ email: '', password: '' });
+        const { email, password } = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password)
+            this.setState({ email: '', password: '' });
+        } catch (error) {
+            console.log(error);
+        }
+
     };
 
     handleChange = event => {
