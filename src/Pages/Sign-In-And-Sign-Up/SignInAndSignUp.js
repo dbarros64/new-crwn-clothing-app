4c7b6c25c8f3ab84f6b6ee9b51376bbd25a9ab8c
Debbie <dbarros64@gmail.com>
2021-05-28 18:35:39 -0700

    updated app with ability to sign-up

--- src/Pages/Sign-In-And-Sign-Up/SignInAndSignUp.js
+++ src/Pages/Sign-In-And-Sign-Up/SignInAndSignUp.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SignIn from '../../Components/SignIn/SignIn';
+import SignUp from '../../Components/SignUp/SignUp';
 
 import './SignInAndSignUp.styles.scss';
 
@@ -7,6 +8,7 @@ import './SignInAndSignUp.styles.scss';
 const SignInAndSignUp = () => (
     <div className='sign-in-and-sign-up'>
         <SignIn />
+        <SignUp />
     </div>
 );
 
